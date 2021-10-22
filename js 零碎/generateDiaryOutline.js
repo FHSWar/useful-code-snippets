df62fbf60e7d848c057c5ead664860c30e6a2f52
FHSWar <fhswar0504@163.com>
2021-10-22 14:51:37 +0800

    做个框框的尖尖

--- js 零碎/generateDiaryOutline.js
+++ js 零碎/generateDiaryOutline.js
@@ -7,7 +7,7 @@ const generateDiaryOutline = (year, path) => { createYearFolder(year, path) }
     year: 指定年份，生成对应年份的日记框子
     path: 指定路径，指定文件夹生成到对应路径
 */
-generateDiaryOutline(2022)
+generateDiaryOutline(2099)
 
 // 创建年份文件夹，输入了年份就用指定的年份，没有就用当年
 function createYearFolder(year = new Date().getFullYear(), path) {
