182ac8661e62fa1447427df7c651c88cb4b106f6
FHSWar <fhswar0504@163.com>
2022-08-20 09:18:47 +0800

    feat: 完善demo

--- serve-history-mode-dist/bootstrap.js
+++ serve-history-mode-dist/bootstrap.js
@@ -7,11 +7,11 @@ const port = 8008;
 http.createServer(function (req, res) {
   // distFolder.serve(req, res);
   distFolder.serve(req, res, function (err, result) {
-    // Fallback for history mode
+    // Fallback for history mode(浏览器要配合开启disable cache嗷)
     if (err !== null && err.status === 404) {
+      console.log('没有对应文件路径的资源', '\nreq.url', req.url) // 'err', err
       // 入口页不命名为index.html会导致加载不到图片，在回落里面从新指向正确的入口页就可以了
-      distFolder.serveFile('/wow.html', 200, {}, req, res);
-      // console.log('err', err, '\nreq.url', req.url)
+      distFolder.serveFile('wow.html', 200, {}, req, res);
     }
   });
 }).listen(port, () => {
